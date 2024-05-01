333d5ebf6a7db6aa5dfc1bf4575e47c07a89463e
KnightOfMelons <ilaprosmotrov@gmail.com>
2024-05-01 18:04:33 +0400

    Forms added

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template  # request, make_response, redirect,
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime, timezone
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from info import KEY
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = KEY
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.now(timezone.utc))
-    # В КНИГЕ БЫЛО НАПИСАНО ТАК, НО ЭТО УСТАРЕВШИЙ ФОРМАТ:
-    # return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>/')
@@ -30,39 +37,10 @@ def internal_server_error(_):
     return render_template('500.html'), 500
 
 
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    # manager.run()
-
-# ==== Закомментированный код, который использовался до этого ====
-
-# from flask_script import Manager
-# manager = Manager(app)
-
-
-# @app.route('/')
-# def index():
-#     return redirect('https://www.youtube.com/')
-
-
-# @app.route('/')
-# def index():
-#     response = make_response('<h1>This document carries a cookie!</h1>')
-#     response.set_cookie('answer', '42')
-#     return response
-
-
-# @app.route('/')
-# def index():
-#     user_agent = request.headers.get('User-Agent')
-#     return '<p>Ypur browser is %s</p>' % user_agent
-
-
-# @app.route('/')
-# def index():
-#     return '<h1>Hello, world!</h1>'
-
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return '<h1>Hello, %s!</h1>' % name
